85fb31678143918ccda54f14de743090251a5cf8
Josh LeBlanc <joshleblanc94@gmail.com>
2025-09-27 13:13:13 +0000

    fix: bust array cache on update

--- app/models/generation.rb
+++ app/models/generation.rb
@@ -2,7 +2,7 @@ class Generation < ApplicationRecord
   after_update_commit -> { broadcast_update_to generation_array }
 
   belongs_to :user
-  belongs_to :generation_array
+  belongs_to :generation_array, touch: true
 
   has_one_attached :image do
     it.variant :thumb, resize_to_limit: [ 256, 256 ], preprocessed: true
