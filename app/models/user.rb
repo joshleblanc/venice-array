c9979d9cd5166811c5f99bb63ef93ed9e7d31321
Joshua LeBlanc <jleblanc@hey.com>
2025-12-31 10:01:22 -0400

    feat: add balances to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  def balances 
+    response = Faraday.new(url: "https://api.venice.ai/api/v1/api_keys/rate_limits") do |faraday|
+      faraday.response :json
+      faraday.adapter Faraday.default_adapter
+    end.get do |request|
+      request.headers["Authorization"] = "Bearer #{venice_api_key}"
+      request.headers["Content-Type"] = "application/json"
+    end
+    
+    if response.status == 200 
+      response.body["data"]["balances"]
+    else 
+      {}
+    end
+  end
 end
